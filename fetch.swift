f9460530207b042a71c2c1e9f84433402d0884bd
aenuros <37673253+aenuros@users.noreply.github.com>
2024-06-17 12:18:21 -0400

    Update fetch.swift

--- fetch.swift
+++ fetch.swift
@@ -1,152 +1,172 @@
+
 import SwiftUI
-import UIKit
 import Vision
 import Photos
 import Foundation
+import class Vision.VNRecognizeTextRequest
 
 struct IdentifiableImage: Identifiable {
     var id = UUID()
-    var image: UIImage
+    var image: CGImage
     var name: String
     var date: Date
 }
 
+guard CommandLine.argc == 3 else {
+    print("Usage: MyCommandLineTool <directory_path> <file_name>")
+    exit(1)
+}
 
-    func createTxt(title: String, _ text: String) {
-        let url = URL( fileURLWithPath: "/Users/name/\(title).txt" )
+let arguments = CommandLine.arguments
+let directoryPath = arguments[1]
+let fileName = arguments[2]
 
-        // write:
-        do {
-          try text.write(to: url, atomically: true, encoding: .utf8)
-        }
-        catch {
-          print("Error writing: \(error.localizedDescription)")
-        }
+var isDir: ObjCBool = false
+let fileManager = FileManager.default
 
-        // read:
-        do {
-          let s = try String( contentsOf: url )
-          print(s)
-        }
-        catch {
-          print("Error thrown while reading file. \(error.localizedDescription)")
-        }
+guard fileManager.fileExists(atPath: directoryPath, isDirectory: &isDir), isDir.boolValue else {
+    print("Error: Directory does not exist at path \(directoryPath)")
+    exit(1)
+}
+
+func createTxt(_ text: String) {
+    let url = URL( fileURLWithPath: "/Users/name/Documents/output/\(fileName).txt" )
+
+    do {
+      try text.write(to: url, atomically: true, encoding: .utf8)
     }
-    
-    func getall(_ labels: [IdentifiableImage]) {
-        var text: [String] = []
-        var currentLabelIndex = 0
+    catch {
+      print("Error writing: \(error.localizedDescription)")
+    }
 
-        func analyze(img: UIImage, completion: @escaping () -> Void) {
-            loading = true
-            DispatchQueue.global().async {
-                // Get the CGImage on which to perform requests.
-                guard let cgImage = img.cgImage else {
-                    loading = false
-                    completion()
-                    return
-                }
+    do {
+      let s = try String( contentsOf: url )
+      print(s)
+    }
+    catch {
+      print("Error thrown while reading file. \(error.localizedDescription)")
+    }
+}
 
-                // Create a new image-request handler.
-                let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+var loading = false
+var identifiables: [IdentifiableImage] = []
 
-                // Create a new request to recognize text.
-                let request = VNRecognizeTextRequest { request, error in
-                    recognizeTextHandler2(request: request, error: error)
-                    completion()
-                }
+func getall(_ labels: [IdentifiableImage]) {
+    var text: [String] = []
+    var currentLabelIndex = 0
 
-                do {
-                    // Perform the text-recognition request.
-                    try requestHandler.perform([request])
-                } catch {
-                    loading = false
-                    completion()
-                }
-            }
+    func analyze(img: CGImage, completion: @escaping () -> Void) {
+        loading = true
+        // Create a new image-request handler.
+        let requestHandler = VNImageRequestHandler(cgImage: img)
+
+        // Create a new request to recognize text.
+        let request = VNRecognizeTextRequest { request, error in
+            recognizeTextHandler2(request: request, error: error)
+            completion()
         }
 
-        func recognizeTextHandler2(request: VNRequest, error: Error?) {
-            guard let observations = request.results as? [VNRecognizedTextObservation] else {
-                return
-            }
-            let recognizedStrings = observations.compactMap { observation in
-                // Return the string of the top VNRecognizedText instance.
-                observation.topCandidates(1).first?.string
-            }
-
-            processResults2(strings: recognizedStrings)
-
+        do {
+            // Perform the text-recognition request.
+            try requestHandler.perform([request])
+        } catch {
             loading = false
+            completion()
         }
-
-        func processResults2(strings: [String]) {
-            for string1 in strings {
-                text.append(string1)
-            }
-        }
-
-        func analyzeNext() {
-            if currentLabelIndex < labels.count {
-                let label = labels[currentLabelIndex]
-                analyze(img: label.image) {
-                    currentLabelIndex += 1
-                    analyzeNext()
-                }
-            } else {
-                print("All analyses completed")
-                createTxt(title: "test3", text.joined(separator: " \n"))
-            }
-        }
-
-        analyzeNext()
     }
+
+    func recognizeTextHandler2(request: VNRequest, error: Error?) {
+        guard let observations = request.results as? [VNRecognizedTextObservation] else {
+            return
+        }
+        let recognizedStrings = observations.compactMap { observation in
+            // Return the string of the top VNRecognizedText instance.
+            observation.topCandidates(1).first?.string
+        }
+
+        processResults2(strings: recognizedStrings)
+        print("recognized", recognizedStrings)
+        loading = false
+    }
+
+    func processResults2(strings: [String]) {
+        for string1 in strings {
+            text.append(string1)
+        }
+    }
+
+    func analyzeNext() {
+        print("doing \(currentLabelIndex)/\(identifiables.count)")
+        if currentLabelIndex < labels.count {
+            let label = labels[currentLabelIndex]
+            analyze(img: label.image) {
+                currentLabelIndex += 1
+                analyzeNext()
+            }
+        } else {
+            print("All analyses completed")
+            createTxt(text.joined(separator: " \n"))
+        }
+    }
+
+    analyzeNext()
+}
+
+func loadPNGImage(from filePath: String) -> CGImage? {
+    guard let dataProvider = CGDataProvider(filename: filePath),
+          let cgImage = CGImage(pngDataProviderSource: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent) else {
+        return nil
+    }
+    return cgImage
+}
+
+func loadPNGsFromDirectory(directoryPath: String) -> [CGImage] {
+    var images = [CGImage]()
+    
+    // Get the file URLs from the directory
+    let fileManager = FileManager.default
+    do {
+        let fileURLs = try fileManager.contentsOfDirectory(at: URL(fileURLWithPath: directoryPath), includingPropertiesForKeys: nil)
         
-    func loadPNGsFromDirectory(directoryPath: String) -> [UIImage] {
-        var images = [UIImage]()
-        
-        let fileManager = FileManager.default
-        do {
-            let fileURLs = try fileManager.contentsOfDirectory(at: URL(fileURLWithPath: directoryPath), includingPropertiesForKeys: nil)
-            
-            for url in fileURLs {
-                if url.pathExtension.lowercased() == "png" {
-                    if let image = UIImage(contentsOfFile: url.path), let date = fileModificationDate(url: url) {
-                            identifiables.append(IdentifiableImage(image: image, name: url.path, date: date))
-                            images.append(image)
-                    }
+        // Filter PNG files and create UIImage objects
+        for url in fileURLs {
+            if url.pathExtension.lowercased() == "png" {
+                if let image = loadPNGImage(from: url.path), let date = fileModificationDate(url: url) {
+                        identifiables.append(IdentifiableImage(image: image, name: url.path, date: date))
+                        images.append(image)
                 }
             }
-        } catch {
-            print("Error while enumerating files \(directoryPath): \(error.localizedDescription)")
         }
-        return images
+    } catch {
+        print("Error while enumerating files \(directoryPath): \(error.localizedDescription)")
     }
     
-    func fileModificationDate(url: URL) -> Date? {
-        do {
-            let attr = try FileManager.default.attributesOfItem(atPath: url.path)
-            return attr[FileAttributeKey.modificationDate] as? Date
-        } catch {
-            return Date(timeIntervalSinceReferenceDate: -123456789.0)
-        }
+    return images
+}
+
+func fileModificationDate(url: URL) -> Date? {
+    do {
+        let attr = try FileManager.default.attributesOfItem(atPath: url.path)
+        return attr[FileAttributeKey.modificationDate] as? Date
+    } catch {
+        return Date(timeIntervalSinceReferenceDate: -123456789.0)
+    }
+}
+
+func openUnfairTest() {
+    _ = loadPNGsFromDirectory(directoryPath: directoryPath)
+    
+    // Print the number of images loaded
+    print("Loaded \(identifiables.count) PNG images.")
+    
+    identifiables.sort {
+        $0.date < $1.date
     }
     
-    func openUnfairTest() {
-        // Example usage
-        let directoryPath = "/Users/name/Documents/unfairtest"
-        _ = loadPNGsFromDirectory(directoryPath: directoryPath)
-        
-        // Print the number of images loaded
-        print("Loaded \(identifiables.count) PNG images.")
-        
-        identifiables.sort {
-            $0.date < $1.date
-        }
-        
-        for identifiable in identifiables {
-            print(identifiable.name)
-        }
-        
-        getall(identifiables)
+    for identifiable in identifiables {
+        print(identifiable.name)
     }
+    print("Total count: ", identifiables.count)
+    getall(identifiables)
+}
+openUnfairTest()
